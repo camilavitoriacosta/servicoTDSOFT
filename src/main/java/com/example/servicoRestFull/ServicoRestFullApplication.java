cfb9a24a997e246bbb54d6bb3bafd6c746de7117
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-05-11 21:48:50 -0400

    inicialização do projeto spring

---
+++ src/main/java/com/example/servicoRestFull/ServicoRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicoRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servicoRestFull/ServicoRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.example.servicoRestFull;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class ServicoRestFullApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
